7ac6d3d0a9702815637d83d2f930068b44a1a6b4
Marcin Tajsner <marcin.tajsner@gmail.com>
2019-03-20 20:50:52 +0100

    Struktura filtrów

--- pingpong/pch.h
+++ pingpong/pch.h
@@ -1,15 +1,10 @@
-﻿// Porady dotyczące rozpoczynania pracy:
-//   1. Użyj okna Eksploratora rozwiązań, aby dodać pliki i zarządzać nimi
-//   2. Użyj okna programu Team Explorer, aby nawiązać połączenie z kontrolą źródła
-//   3. Użyj okna Dane wyjściowe, aby sprawdzić dane wyjściowe kompilacji i inne komunikaty
-//   4. Użyj okna Lista błędów, aby zobaczyć błędy
-//   5. Wybierz pozycję Projekt > Dodaj nowy element, aby utworzyć nowe pliki kodu, lub wybierz pozycję Projekt > Dodaj istniejący element, aby dodać istniejące pliku kodu do projektu
-//   6. Aby w przyszłości ponownie otworzyć ten projekt, przejdź do pozycji Plik > Otwórz > Projekt i wybierz plik sln
-
-#ifndef PCH_H
+﻿#ifndef PCH_H
 #define PCH_H
 
 #include <iostream>
+#include <SFML/System.hpp>
+#include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
 
-#endif //PCH_H
+#endif 
